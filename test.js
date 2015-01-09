4f6803944bc9c9e900e2017441d2f06732c8b3d7
bcoe <bencoe@gmail.com>
2015-01-08 23:55:50 -0800

    added option of wrapping cheerio with cheerio-eq. Added support for :first, :last

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var test = require('tape');
-var cheerio = require('cheerio');
 var find = require('./');
+var cheerio = find.wrap(require('cheerio'));
 
 var html = '<div>foo</div><div>bar</div>';
 var selector = 'div:eq(1)';
@@ -10,27 +10,41 @@ var selector = 'div:eq(1)';
 test('Non-eq selector', function (t) {
   var html = '<div>foo</div><div>bar</div>';
   var $ = cheerio.load(html);
-  t.equal(find($, 'div').text(), 'foobar');
+  t.equal($('div').text(), 'foobar');
   t.end();
 });
 
 test('Simple selector ending in :eq()', function (t) {
   var html = '<div>foo</div><div>bar</div>';
   var $ = cheerio.load(html);
-  t.equal(find($, 'div:eq(1)').text(), 'bar');
+  t.equal($('div:eq(1)').text(), 'bar');
   t.end();
 });
 
 test('Simple selector with :eq() in the middle', function (t) {
   var html = '<div><span>foo</span></div><div><span>bar</span></div>';
   var $ = cheerio.load(html);
-  t.equal(find($, 'div:eq(0) span').text(), 'foo');
+  t.equal($('div:eq(0) span').text(), 'foo');
   t.end();
 });
 
 test('Complex selector', function (t) {
   var html = '<div><span><h1>foo</h1></span><span><h1>bar</h1></span></div>';
   var $ = cheerio.load(html);
-  t.equal(find($, 'div:eq(0) span:eq(1) h1').text(), 'bar');
+  t.equal($('div:eq(0) span:eq(1) h1').text(), 'bar');
+  t.end();
+});
+
+test(':first', function(t) {
+  var html = '<div><span><h1>foo</h1></span><span><h1>bar</h1></span></div>';
+  var $ = cheerio.load(html);
+  t.equal($('div span:first').text(), 'foo');
+  t.end();
+});
+
+test(':last', function(t) {
+  var html = '<div><span><h1>foo</h1></span><span><h1>bar</h1></span></div>';
+  var $ = cheerio.load(html);
+  t.equal($('span:last').text(), 'bar');
   t.end();
 });
